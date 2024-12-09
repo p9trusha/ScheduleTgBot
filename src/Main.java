b68ba1194482a51f5abad07bd0a06ede29643677
p9trusha <petrnevolin2005@gmail.com>
2024-12-09 20:06:41 +0300

    etu api

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+class EtuApi {
+    String getJson(String group) throws IOException {
+        group = URLEncoder.encode(group, StandardCharsets.UTF_8);
+        String apiLink = String.format(
+                "https://https://digital.etu.ru/api/mobile/schedule",
+                group
+        );
+        URL searchReguest = new URL(apiLink);
+        URLConnection urlCon = searchReguest.openConnection();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        urlCon.getInputStream()));
+        String inputLine;
+        inputLine = in.readLine();
+        in.close();
+        return (inputLine);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ src/Parser.java
@@ -0,0 +1,22 @@
+public class Parser {
+    void json(String groupNumber, String inputLine) {
+        String TagGroupNumber = groupNumber;
+        String TagGroup = "group";
+        String TagDays = "days";
+        String[] TagsNumberOfDay = new String[7];
+        for (int i = 0; i < 7; i++) {
+            TagsNumberOfDay[i] = String.valueOf(i);
+        }
+        String TagLessons = "lessons";
+        String TagTeacher = "teacher";
+        String TagSecondTeacher = "second_teacher";
+        String TagSubjectType = "subjectType";
+        String TagWeek = "week";
+        String TagName = "name";
+        String TagStartTime = "start_time";
+        String TagEndTime = "end_time";
+        String TagRoom = "room";
+        String TagIsDistant = "is_distant";
+        String TagURL = "url";
+    }
+}
