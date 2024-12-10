353a0c8d3c47b7d18452911787d0245da4466aec
Collector667 <146832679+Collector667@users.noreply.github.com>
2024-12-10 21:57:45 +0300

    Update project structure

---
+++ src/scheldule/telegramBot/BotCommands.java
@@ -0,0 +1,26 @@
+package scheldule.telegramBot;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+
+@Getter
+public class BotCommands {
+    /**commands like schedule tomorrow 3354
+     * schedule
+     */
+    public ArrayList<String> oneStepCommand = new ArrayList<>();
+    /**
+     * commands like schedule tomorrow
+     * group?
+     * 3354
+     * schedule
+     */
+    public ArrayList<String> twoStepCommand = new ArrayList<>();
+    public void addOneStepCommand(String s) {
+        this.oneStepCommand.add(s);
+    }
+    public void addTwoStepComand(String s) {
+        this.twoStepCommand.add(s);
+    }
+}
